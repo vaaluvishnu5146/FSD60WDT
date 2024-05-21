d33b228a07bc63e3a759fa51e74b0d9f078ecdd7
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-05-21 19:23:23 +0530

    V2 code for findMax

--- findMax.js
+++ findMax.js
@@ -1,7 +1,10 @@
+/**
+ * Version = 2.0.0
+ */
 var arr = [1, 4, 2, 6, 10];
-var max = -1;
+var max = arr[0];
 
-for (var x = 0; x < arr.length; x++) {
+for (var x = 1; x < arr.length; x++) {
   if (arr[x] > max) max = arr[x];
 }
 
