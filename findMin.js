e478acb372cea1b27b114b751d8f391e76dab262
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-05-21 20:00:57 +0530

    findMin V2

--- findMin.js
+++ findMin.js
@@ -1,7 +1,8 @@
+// V2
 var arr = [1, 4, 0, 2, 6]; // 0
-var min = 100;
+var min = arr[0];
 
-for (var x = 0; x < arr.length; x++) {
+for (var x = 1; x < arr.length; x++) {
   if (min > arr[x]) min = arr[x];
 }
 
