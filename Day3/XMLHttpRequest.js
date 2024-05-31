dc947cd387b68a3eb1879f85521d931ae023d93d
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-05-31 20:56:03 +0530

    Added countries api integration

--- Day3/XMLHttpRequest.js
+++ Day3/XMLHttpRequest.js
@@ -1,12 +1,21 @@
-var uri = "https://jsonplaceholder.typicode.com/todos/";
+var uri = "https://restcountries.com/v3.1/all"; // "https://jsonplaceholder.typicode.com/todos/";
 // Initialize New XMLHttpRequest instance
 var newInstance = new XMLHttpRequest();
 newInstance.open("GET", uri);
 newInstance.send();
 newInstance.onreadystatechange = function () {
   if (this.status === 200 && this.readyState === 4) {
-    var todos = JSON.parse(this.response);
-    console.log(todos);
+    var countries = JSON.parse(this.response);
+    // countries.forEach((country) =>
+    //   console.log(country.name.common, country.population)
+    // );
+    var totalPopulation = countries.reduce((prev, current) => {
+      if (typeof prev === "object") {
+        return prev.population + current.population;
+      }
+      return prev + current.population;
+    });
+    console.log("Population", totalPopulation);
   }
 };
 newInstance.onerror = function (error) {
